b22ebbdc639f1aea5d0ae0ec755246b652de5340
Ángel Rubén Pantoja Ortiz <apo.disenoweb@gmail.com>
2020-03-10 18:31:09 -0600

    fix - valor membresía

--- src/components/membership/MembershipList.js
+++ src/components/membership/MembershipList.js
@@ -123,7 +123,7 @@ class MembershipList extends React.Component {
         };
 
         const listItems = this.state.list.map((membership) =>{
-            if(combo.hide_in_front){
+            if(membership.hide_in_front){
                 if(membership.hide_in_front === false || membership.hide_in_front === 0){
                     if(
                         this.state.weAreHome === false && membership.status === 'active' ||
