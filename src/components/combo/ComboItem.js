fbbc5dd654345a89200b19461d709ed256178da3
Wisquimas <wisquimas@gmail.com>
2021-09-01 11:00:17 -0500

    fixCurrency

--- src/components/combo/ComboItem.js
+++ src/components/combo/ComboItem.js
@@ -8,152 +8,158 @@ import GafaFitSDKWrapper from "../utils/GafaFitSDKWrapper";
 import {formatMoney} from "../utils/FormatUtils";
 
 class ComboItem extends React.Component {
-   constructor(props) {
-      super(props);
+    constructor(props) {
+        super(props);
 
-      this.state = {
-         currentBrand: null,
-         currentLocation: null,
-      }
-   }
+        this.state = {
+            currentBrand: null,
+            currentLocation: null,
+        }
+    }
 
-   componentDidMount(){
-      let {combo} = this.props;
-      let locations = GlobalStorage.get('locations');
+    componentDidMount() {
+        let {combo} = this.props;
+        let locations = GlobalStorage.get('locations');
 
-      if(locations){
-         locations = locations.filter(function(location){
-            return combo.brand.id === location.brand.id
-         })
-      }
+        if (locations) {
+            locations = locations.filter(function (location) {
+                return combo.brand.id === location.brand.id
+            })
+        }
 
-      this.setState({
-         currentBrand: locations[0].brand,
-         currentLocation: locations[0],
-      })
-   }
+        this.setState({
+            currentBrand: locations[0].brand,
+            currentLocation: locations[0],
+        })
+    }
 
-   handleClick(event) {
-      event.preventDefault();
-      let currentElement = this;
+    handleClick(event) {
+        event.preventDefault();
+        let currentElement = this;
 
-      GafaFitSDKWrapper.isAuthenticated(function(auth){
-         if (auth) {
-            currentElement.showBuyFancyForLoggedUsers();
-         } else {
-            currentElement.showLoginForNotLoggedUsers();
-         }
-      });
-   };
-
-   showBuyFancyForLoggedUsers() {
-      let comp = this;
-      let {combo} = this.props;
-      let {currentBrand, currentLocation} = this.state
-
-      comp.setState({
-         openFancy: true,
-      });
-
-      const fancy = document.querySelector('[data-gf-theme="fancy"]');
-      fancy.classList.add('active');
-
-      setTimeout(function(){
-         fancy.classList.add('show');
-      }, 400);
-
-      GafaFitSDKWrapper.getFancyForBuyCombo(
-         currentBrand.slug,
-         currentLocation.slug,
-         combo.id, 
-         function (result) {
-            getFancy();
-
-            function getFancy(){
-               if(document.querySelector('[data-gf-theme="fancy"]').firstChild){
-                  const closeFancy = document.getElementById('CreateReservationFancyTemplate--Close');
-                  
-                  closeFancy.addEventListener('click', function(e){
-                     fancy.removeChild(document.querySelector('[data-gf-theme="fancy"]').firstChild);
-
-                     fancy.classList.remove('show');
-
-                     setTimeout(function(){
-                        fancy.classList.remove('active');
-                        fancy.innerHTML = '<div class="spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>';
-      
-                     }, 400);
-
-                     comp.setState({
-                        openFancy: false,
-                     })
-                  })
-               } else {
-                  setTimeout(getFancy, 1000);
-               }
+        GafaFitSDKWrapper.isAuthenticated(function (auth) {
+            if (auth) {
+                currentElement.showBuyFancyForLoggedUsers();
+            } else {
+                currentElement.showLoginForNotLoggedUsers();
             }
-         }
-      );
-   }
+        });
+    };
 
-   showLoginForNotLoggedUsers() {
-      let comp = this;
-      let locations = GlobalStorage.get('locations');
-      locations = locations.filter(function(location){ return location.brand.slug === comp.props.combo.brand.slug});
+    showBuyFancyForLoggedUsers() {
+        let comp = this;
+        let {combo} = this.props;
+        let {currentBrand, currentLocation} = this.state
 
-      window.GFtheme.combo_id = this.props.combo.id;
-      window.GFtheme.brand_slug = this.props.combo.brand.slug;
-      window.GFtheme.location_slug = locations[0].slug;
+        comp.setState({
+            openFancy: true,
+        });
 
-      this.props.setShowRegister(true);
-   }
+        const fancy = document.querySelector('[data-gf-theme="fancy"]');
+        fancy.classList.add('active');
 
-   render() {
-      let preC = 'GFSDK-c';
-      let preE = 'GFSDK-e';
-      let productClass = preE + '-product';
-      let comboClass = preC + '-membershipList';
-      let {combo} = this.props;
-      let {openFancy} = this.state;
-      // const services = this.getServicesAndParentsForCombo();
+        setTimeout(function () {
+            fancy.classList.add('show');
+        }, 400);
 
-      return (
-         <div className={comboClass + 'item ' + productClass}>
-            <div className={productClass + '__head'}>
-               <h3 className="this-name">{this.props.combo.name}</h3>
-            </div>
-            <div className={productClass + '__body'}>
-               {this.props.combo.has_discount &&
-                  <div>
-                     <div className="this-price has-discount">
+        GafaFitSDKWrapper.getFancyForBuyCombo(
+            currentBrand.slug,
+            currentLocation.slug,
+            combo.id,
+            function (result) {
+                getFancy();
+
+                function getFancy() {
+                    if (document.querySelector('[data-gf-theme="fancy"]').firstChild) {
+                        const closeFancy = document.getElementById('CreateReservationFancyTemplate--Close');
+
+                        closeFancy.addEventListener('click', function (e) {
+                            fancy.removeChild(document.querySelector('[data-gf-theme="fancy"]').firstChild);
+
+                            fancy.classList.remove('show');
+
+                            setTimeout(function () {
+                                fancy.classList.remove('active');
+                                fancy.innerHTML = '<div class="spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>';
+
+                            }, 400);
+
+                            comp.setState({
+                                openFancy: false,
+                            })
+                        })
+                    } else {
+                        setTimeout(getFancy, 1000);
+                    }
+                }
+            }
+        );
+    }
+
+    showLoginForNotLoggedUsers() {
+        let comp = this;
+        let locations = GlobalStorage.get('locations');
+        locations = locations.filter(function (location) {
+            return location.brand.slug === comp.props.combo.brand.slug
+        });
+
+        window.GFtheme.combo_id = this.props.combo.id;
+        window.GFtheme.brand_slug = this.props.combo.brand.slug;
+        window.GFtheme.location_slug = locations[0].slug;
+
+        this.props.setShowRegister(true);
+    }
+
+    render() {
+        let preC = 'GFSDK-c';
+        let preE = 'GFSDK-e';
+        let productClass = preE + '-product';
+        let comboClass = preC + '-membershipList';
+        let {combo} = this.props;
+        let {openFancy} = this.state;
+        // const services = this.getServicesAndParentsForCombo();
+        let currency = GlobalStorage.getBrandCurrency(combo.brand);
+
+        return (
+            <div className={comboClass + 'item ' + productClass}>
+                <div className={productClass + '__head'}>
+                    <h3 className="this-name">{this.props.combo.name}</h3>
+                </div>
+                <div className={productClass + '__body'}>
+                    {this.props.combo.has_discount &&
+                    <div>
+                        <div className="this-price has-discount">
+                            <p>
+                                {currency.prefijo}{formatMoney(this.props.combo.price, 0)} {currency.sufijo}
+                            </p>
+                        </div>
+                    </div>
+                    }
+                    <div className="this-price has-total">
                         <p>
-                           ${formatMoney(this.props.combo.price, 0)} MXN
+                            {currency.prefijo}{formatMoney(this.props.combo.price_final, 0)} {currency.sufijo}
                         </p>
-                     </div>
-                  </div>
-               }
-               <div className="this-price has-total">
-                  <p>
-                     ${formatMoney(this.props.combo.price_final, 0)} MXN
-                  </p>
-               </div>
+                    </div>
 
-               {this.props.combo.short_description &&
-                  <p className={'this-shortDescription'}>{this.props.combo.short_description}</p>
-               }
+                    {this.props.combo.short_description &&
+                    <p className={'this-shortDescription'}>{this.props.combo.short_description}</p>
+                    }
 
-               <button style={{ pointerEvents: openFancy ? 'none' : 'auto' }} className="buq-accentColor" onClick={openFancy ? null :this.handleClick.bind(this)}> Comprar </button>
+                    <button style={{pointerEvents: openFancy ? 'none' : 'auto'}} className="buq-accentColor"
+                            onClick={openFancy ? null : this.handleClick.bind(this)}> Comprar
+                    </button>
 
+                </div>
+                <div className={productClass + '__footer'}>
+                    {combo.expiration_days
+                        ? <p className={'this-expiration'}><span>{Strings.EXPIRE_IN}</span>
+                            <strong>{combo.expiration_days} {Strings.DAYS}</strong></p>
+                        : null
+                    }
+                </div>
             </div>
-            <div className={productClass + '__footer'}>
-               {combo.expiration_days
-                  ?   <p className={'this-expiration'}><span>{Strings.EXPIRE_IN}</span> <strong>{combo.expiration_days} {Strings.DAYS}</strong></p>
-                  :   null
-               }
-            </div>
-         </div>
-      );
-   }
+        );
+    }
 }
 
 export default ComboItem;
--- src/components/store/GlobalStorage.js
+++ src/components/store/GlobalStorage.js
@@ -1,128 +1,158 @@
 import GafaFitSDKWrapper from "../utils/GafaFitSDKWrapper";
 
 const GlobalStorage = {
-   listeners: [],
-   segmentedListeners: [],
-   me: null,
-   wallet: null,
+    listeners: [],
+    segmentedListeners: [],
+    me: null,
+    wallet: null,
 
-   //Brands Controller
-   brands: [],
-   currentBrand: null,
-   
-   //Location Controller
-   locations: [],
+    //Brands Controller
+    brands: [],
+    currentBrand: null,
 
-   //Combos
-   combos: [],
-   
-   //Memberships
-   memberships: [],
-   
-   //Staff
-   staff:[],
-   
-   //Staff
-   services:[],
-   
-   //Rooms
-   rooms:[],
-    
-   //PaymentMethod Controller
-   ConektaPaymentInfo: null,
-   ConektaPaymentNotification: null,
-   ConektaPaymentError: null,
+    //Location Controller
+    locations: [],
 
+    //Combos
+    combos: [],
 
-   //Profile Filter
-   filter_location: '',
-   filter_brand: '',
+    //Memberships
+    memberships: [],
+
+    //Staff
+    staff: [],
+
+    //Staff
+    services: [],
+
+    //Rooms
+    rooms: [],
+
+    //PaymentMethod Controller
+    ConektaPaymentInfo: null,
+    ConektaPaymentNotification: null,
+    ConektaPaymentError: null,
 
 
-   //Future Class
-   future_classes: null,
-   past_classes: null,
-   purchase: null,
+    //Profile Filter
+    filter_location: '',
+    filter_brand: '',
 
-   get(property) {
-      return this[property];
-   },
 
-   find(property, id) {
-      if (Array.isArray(this[property])) {
-         return this[property].find(o => o.id === parseInt(id));
-      }
-   },
+    //Future Class
+    future_classes: null,
+    past_classes: null,
+    purchase: null,
 
-   set(property, value, cb) {
-      this[property] = value ? value : null;
-      this.TriggerChange(cb, property);
-   },
+    get(property) {
+        return this[property];
+    },
+    /**
+     *
+     * @param brand
+     * @returns {*|{sufijo, prefijo}}
+     */
+    getBrandCurrency(brand){
+        let currencyId = !!brand && brand.hasOwnProperty('currencies_id') ? brand.currencies_id : 1;
+        return GlobalStorage.getCurrency(currencyId);
+    },
+    getCurrency(currency_id){
+        switch (currency_id) {
+            case 2:
+                return {
+                    sufijo: 'USD',
+                    prefijo: '$',
+                };
+                break;
+            case 3:
+                return {
+                    sufijo: 'EUR',
+                    prefijo: '€',
+                };
+                break;
+            default:
+                return {
+                    sufijo: 'MXN',
+                    prefijo: '$',
+                };
+                break
+        }
+    },
+    find(property, id) {
+        if (Array.isArray(this[property])) {
+            return this[property].find(o => o.id === parseInt(id));
+        }
+    },
 
-   push(property, value, cb) {
-      if (Array.isArray(this[property])) {
-         if (Array.isArray(value)) {
-               this[property] = this[property].concat(value);
-         } else {
-               this[property].push(value);
-         }
-         this.TriggerChange(cb, property);
-      }
-   },
+    set(property, value, cb) {
+        this[property] = value ? value : null;
+        this.TriggerChange(cb, property);
+    },
 
-   addListener(callback) {
-      this.listeners.push(callback);
-   },
+    push(property, value, cb) {
+        if (Array.isArray(this[property])) {
+            if (Array.isArray(value)) {
+                this[property] = this[property].concat(value);
+            } else {
+                this[property].push(value);
+            }
+            this.TriggerChange(cb, property);
+        }
+    },
 
-   initialValues(brands, cb){
-      let curStore = this;
-      let currentBrand = brands[0];
-      let locations = [];
-      let rooms = [];
-      let brandList = !window.GFtheme.BrandID ? brands : brands.filter(brand => brand.id === window.GFtheme.BrandID);
+    addListener(callback) {
+        this.listeners.push(callback);
+    },
 
-      let loop = 0;
+    initialValues(brands, cb){
+        let curStore = this;
+        let currentBrand = brands[0];
+        let locations = [];
+        let rooms = [];
+        let brandList = !window.GFtheme.BrandID ? brands : brands.filter(brand => brand.id === window.GFtheme.BrandID);
 
-      console.log(brandList);
+        let loop = 0;
 
-      if(brandList){
-         this.brands = brandList;
-         this.currentBrand = currentBrand;
-      }
+        console.log(brandList);
 
-      brandList.forEach(function(brand){
-         GafaFitSDKWrapper.getBrandLocationsWithoutBrand(brand.slug, {}, function (result) {
-            locations = locations.concat(result.data);
+        if (brandList) {
+            this.brands = brandList;
+            this.currentBrand = currentBrand;
+        }
 
-            GafaFitSDKWrapper.getBrandRooms(brand.slug, {per_page: 1000}, function(result){
-               rooms = rooms.concat(result.data);
-               loop++;
+        brandList.forEach(function (brand) {
+            GafaFitSDKWrapper.getBrandLocationsWithoutBrand(brand.slug, {}, function (result) {
+                locations = locations.concat(result.data);
 
-               if(loop === brandList.length){
-                  GlobalStorage.locations = locations;
-                  GlobalStorage.rooms = rooms;
-                  if(cb){
-                     cb();
-                  }
-               }
+                GafaFitSDKWrapper.getBrandRooms(brand.slug, {per_page: 1000}, function (result) {
+                    rooms = rooms.concat(result.data);
+                    loop++;
+
+                    if (loop === brandList.length) {
+                        GlobalStorage.locations = locations;
+                        GlobalStorage.rooms = rooms;
+                        if (cb) {
+                            cb();
+                        }
+                    }
+                });
             });
-         });
-      });
-   },
+        });
+    },
 
-   addSegmentedListener(segment, callback) {
-      if (segment && callback) {
-         if (segment.length) {
-               //array
-               segment.forEach(function (singleSegment) {
-                  GlobalStorage.subscribeToSegment(singleSegment, callback)
-               })
-         } else {
-               //no array
-               GlobalStorage.subscribeToSegment(segment, callback)
-         }
-      }
-   },
+    addSegmentedListener(segment, callback) {
+        if (segment && callback) {
+            if (segment.length) {
+                //array
+                segment.forEach(function (singleSegment) {
+                    GlobalStorage.subscribeToSegment(singleSegment, callback)
+                })
+            } else {
+                //no array
+                GlobalStorage.subscribeToSegment(segment, callback)
+            }
+        }
+    },
 
     subscribeToSegment(segment, callback) {
         if (!this.segmentedListeners.hasOwnProperty(segment)) {
